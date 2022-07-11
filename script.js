fe12bca9f27d1a97117f68ef475dee098bdd8198
Jesse <jessejkilback@gmail.com>
2022-07-10 20:24:41 -0700

    Change Layout

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.grid');
 console.log(container);
 
-for (i=0; i<256; i++){
+for (i=0; i<2500; i++){
 const newDiv = document.createElement('div');
 newDiv.classList.add('glow');
+//newDiv.addEventListener('mouseDown', func)
 container.appendChild(newDiv);
+}
+
+const pixels = document.querySelectorAll('.glow');
+
+//document.querySelectorAll('.glow').forEach(item => {
+//    item.addEventListener('mouseenter', event => {
+//    this.css("backgroundColor", "black")    }
+//)})
+for (i=0;i<pixels.length;i++){
+    pixels[i].addEventListener('mouseenter', event => {
+       console.log(this);
+       console.log(typeof(this));
+       event.target.setAttribute('class','black');
+        // this.classList.add('black');
+    })
 }
