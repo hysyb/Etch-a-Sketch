7d648ead78c76b8c5c77b64bc1e28863ce9b703c
Jesse <jessejkilback@gmail.com>
2022-08-18 10:39:36 -0700

    add mouseover to paint. remove draggabilty from grid

--- script.js
+++ script.js
@@ -7,16 +7,42 @@ newDiv.classList.add('glow');
 //newDiv.addEventListener('mouseDown', func)
 container.appendChild(newDiv);
 }
-
 const pixels = document.querySelectorAll('.glow');
+
+
+container.addEventListener('dragstart', (e) => {
+  e.preventDefault()
+})
+container.addEventListener('drop', (e) => {
+  e.preventDefault()
+})
 
 //document.querySelectorAll('.glow').forEach(item => {
 //    item.addEventListener('mouseenter', event => {
 //    this.css("backgroundColor", "black")    }
 //)})
-for (i=0;i<pixels.length;i++){
-    pixels[i].addEventListener('mouseenter', changeColor)
+let mouseIsDown = false;
+document.body.addEventListener('mousedown', toggleMouseState);
+document.body.addEventListener('mouseup', toggleMouseState);
+function toggleMouseState(){
+ if (mouseIsDown == true){
+    mouseIsDown=false;
+    console.log(mouseIsDown);
+ }
+    else {
+        mouseIsDown=true;
+        console.log(mouseIsDown);
     }
+}
+
+for (i=0;i<pixels.length;i++){
+    pixels[i].addEventListener('mouseover', changeColor)
+    }
+    for (i=0;i<pixels.length;i++){
+        pixels[i].addEventListener('click', changeColor)
+        }    
 function changeColor(e){
-    e.target.style.backgroundColor = 'black'
+    if (mouseIsDown == true){
+        e.target.style.backgroundColor = 'black'
+    }
 }
